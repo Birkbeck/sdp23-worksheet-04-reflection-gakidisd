1531f84ffa4504915e9eb3d681a0c0eee0b4f865
dimgak <gakidisd@gmail.com>
2023-02-15 21:15:25 +0000

    Test 1 Test 2 Test 3

--- test/reflection/Autograder.java
+++ test/reflection/Autograder.java
@@ -1,20 +1,50 @@
 package reflection;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
 
 public class Autograder {
 
-    static final String CLASS_NAME = ""; // TODO: insert the class name to be tested
+    static final String CLASS_NAME = "StudentClass.java"; // TODO: insert the class name to be tested
+
+    StudentClass student = new StudentClass();
+    Class<?> c = student.getClass();
+
+    public Autograder() throws ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
+    }
+
 
     @Test
     public void testMoreThanFourFields() {
         // TODO: add testing code
+        Field[] fields = c.getDeclaredFields();
+        System.out.println(Arrays.toString(fields));
+        System.out.println(c);
+        Assertions.assertFalse(fields.length < 4);
     }
 
     @Test
     public void testNonPrivateFields() {
         // TODO: add testing code
+        Field[] fields = c.getDeclaredFields();
+        for (Field field : fields){
+            Assertions.assertFalse(Modifier.isPublic(field.getModifiers()));
+
+        }
     }
 
     // TODO: add a test for each of the remaining items of Question 3
+    @Test
+    public void testArray(){
+        Field[] fields = c.getDeclaredFields();
+        for (Field field: fields){
+            Assertions.assertFalse(field.getType().isArray());
+        }
+    }
 }
---
+++ test/reflection/StudentClass.java
@@ -0,0 +1,18 @@
+package reflection;
+
+import javax.swing.*;
+
+public class StudentClass {
+    private String f1;
+    private String f2;
+    private int f3;
+    private double f4;
+    private JButton f5;
+//    private String[] f6;
+
+    public StudentClass(){
+
+    }
+
+
+}
